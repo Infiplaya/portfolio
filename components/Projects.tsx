618c18c83ab03bd4d6803df54c13d227d28bed30
Pawel Sobaniec <infipablosoban@gmail.com>
2022-10-05 19:13:35 +0200

    descriptions added

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
       <h1 className="font-semibold text-3xl text-black dark:text-white tracking-wider">
         Projects
       </h1>
-      <div className="mt-5 flex gap-10">
+      <div className="mt-5 flex gap-10 w-full h-full">
         <Link href="https://grabafood.vercel.app/">
           <a className="w-1/2">
             <Image
@@ -22,13 +22,13 @@ export default function Projects() {
             ></Image>
           </a>
         </Link>
-        <div className="w-1/2 flex flex-col gap-5">
+        <div className="w-1/2 flex flex-col">
           <Link href="https://grabafood.vercel.app/">
             <h1 className="cursor-pointer text-2xl font-semibold dark:text-blue-500 text-blue-700">
               Grab A Food
             </h1>
           </Link>
-          <p className="mb-auto">Description</p>
+          <p className="mb-auto">Web application that will help you try new food</p>
           <Link href="https://github.com/Infiplaya/grabafood">
             <a className="dark:text-blue-500 text-blue-700 dark:hover:text-blue-700 hover:text-blue-300">
               Github
@@ -48,13 +48,13 @@ export default function Projects() {
             ></Image>
           </a>
         </Link>
-        <div className="w-1/2 flex flex-col justify-between gap-4">
+        <div className="w-1/2 flex flex-col">
           <Link href="https://quizgame-1xt4.vercel.app/">
             <h1 className="cursor-pointer text-2xl font-semibold dark:text-blue-500 text-blue-700">
               Trivia quiz
             </h1>
           </Link>
-          <p className="mb-auto">Description</p>
+          <p className="mb-auto">Interactive quiz, where you can test your knowledge</p>
           <Link href="https://github.com/Infiplaya/quizgame">
             <a className="dark:text-blue-500 text-blue-700 dark:hover:text-blue-700 hover:text-blue-300">
               Github
@@ -80,7 +80,7 @@ export default function Projects() {
               Portfolio
             </h1>
           </Link>
-          <p className="mb-auto">Description</p>
+          <p className="mb-auto">This is where you are. Place where you can learn more about me.</p>
           <Link href="https://github.com/Infiplaya/portfolio">
             <a className="dark:text-blue-500 text-blue-700 dark:hover:text-blue-700 hover:text-blue-300">
               Github

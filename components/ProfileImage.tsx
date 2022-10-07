aa64f2857f368a7cac8dadf0587820eab35ac17d
Pawel Sobaniec <infipablosoban@gmail.com>
2022-10-07 13:45:12 +0200

    updated profile picture

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,14 +1,12 @@
 import Image from "next/future/image";
-import profileImg from "../public/profile.png";
+import profileImg from "../public/profilepic.png";
 
 export default function ProfileImg() {
   return (
     <Image
       src={profileImg}
       alt="My picture"
-      width={150}
-      height={150}
-      className="self-center border-solid border-2 border-blue-500 mb-5 rounded-full"
+      className="self-center border-solid border-4 border-blue-500 mb-5 rounded-full w-8/12 sm:w-56"
     ></Image>
   );
 }
